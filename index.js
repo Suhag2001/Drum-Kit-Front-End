dfbd4836bba203770b4df92b373c7c85af3c08f9
= <=>
2023-08-23 22:46:35 +0530

    return the changes

--- index.js
+++ index.js
@@ -16,27 +16,27 @@ document.addEventListener("keypress",function(event){
 
 function makeSound(key){
     switch (key) {
-        case "a":
+        case "w":
             var tom1 = new Audio("sounds/tom-1.mp3")
             tom1.play()
             break;
 
-        case "s":
+        case "a":
             var tom2 = new Audio("sounds/tom-2.mp3")
             tom2.play()
             break;
 
-        case "d":
+        case "s":
             var tom3 = new Audio("sounds/tom-3.mp3")
             tom3.play()
             break;
 
-        case "f":
+        case "d":
             var tom4 = new Audio("sounds/tom-4.mp3")
             tom4.play()
             break;
 
-        case "l":
+        case "j":
             var snare = new Audio("sounds/snare.mp3")
             snare.play()
             break;
@@ -46,7 +46,7 @@ function makeSound(key){
             crash.play()
             break;
 
-        case "j":
+        case "l":
             var kick = new Audio("sounds/kick-bass.mp3")
             kick.play();
             break;
